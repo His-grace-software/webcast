df3d2ed355942cb96a8b16bee16e14a4a7c2128c
Odueso Emmanuel <emmanuelodueso@gmail.com>
2023-02-25 13:15:15 +0100

    Delete README.php

--- README.php
+++
@@ -1,6 +0,0 @@
-1:Move the folder to localhost
-2:create a database name in xampp mysql with 'dclmwebcastdb'
-3: import the databse file from the database folder 'db'
-4. Usign localhost navigate usig the url to dclm_webcast
-5: Get atleast 4 videos and paste them in the video folder which is inside the assets folder
-6: Name each of the video with the following name: vid1, vid2, vid3, vid4 respectively
